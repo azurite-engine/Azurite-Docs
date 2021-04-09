b67e744fbb2595ebe4f0d1b9c4ac551c0a5dc4d9
Asher <asher.haun@outlook.com>
2021-04-09 11:58:56 -0500

    fix legend

--- js/insert/legendTuts.js
+++ js/insert/legendTuts.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-/** 
- * Contains the links that are inserted into the HTML on load for tutorials website
- */
-
-let legend = `
-<h3>Basics</h3>
-<nav>
-    <ul>
-        <!-- Pages -->
-        <li><a href="${lp}/tutorials/tutorials.html">Introduction</a></li>
-        <li><a href="${lp}/tutorials/set-a-project.html">Set up a project</a></li>
-    </ul>
-</nav>
-
-<!-- <h3>Azurite elements</h3> -->
-<nav>
-    <ul>
-        <!-- Pages -->
-        
-    </ul>
-</nav>
-
-<h3>Advanced</h3>
-<nav>
-    <ul>
-        <li><a href="${lp}/tutorials/custom-renderer.html">A Custom Renderer</a></li>
-    </ul>
-</nav>
-
-<h3>Contributing</h3>
-<nav>
-    <ul>
-        <!-- Pages -->
-        <li><a href="${lp}/tutorials/creating-docs.html">Creating docs</a></li>
-    </ul>
-</nav>
-`;
-
-function add (selector, html) {
-    document.querySelector(selector).innerHTML += html;
-}
-
-
-add('.legend-card', legend);
-
-
-
-
-
-=======
 /** 
  * Contains the links that are inserted into the HTML on load for tutorials website
  */
@@ -99,4 +48,3 @@ add('.legend-card', legend);
 
 
 
->>>>>>> c7e00d4d07ccb1c81743229f7b7c46a815cff1b3
